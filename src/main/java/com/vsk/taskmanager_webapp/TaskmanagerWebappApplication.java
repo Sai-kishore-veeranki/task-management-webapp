3d6cfea773baa7f43dfdf6c3c0d968066973eae1
sai kishore veeranki <saikishoreveeranki@gmail.com>
2025-07-01 23:57:50 +0530

    Add all project files for initial setup

---
+++ src/main/java/com/vsk/taskmanager_webapp/TaskmanagerWebappApplication.java
@@ -0,0 +1,13 @@
+package com.vsk.taskmanager_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsk/taskmanager_webapp/TaskmanagerWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsk.taskmanager_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
